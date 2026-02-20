ab4bcaac7e8290d399cefb7afe53fe0ceda4f42c
jobscale <jobscale@users.noreply.github.com>
2026-02-20 12:39:59 +0900

    port updated

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ import fs from 'fs';
 import { logger } from '@jobscale/logger';
 import { app, errorHandler } from './app/index.js';
 
+const BIND = process.env.BIND || '0.0.0.0';
 const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
+const SPORT = Number.parseInt(process.env.SPORT, 10) || 3443;
 
 const httpServer = () => {
   const server = http.createServer((req, res) => app(req, res));
   server.on('error', (e, req, res) => errorHandler(e, req, res));
-  const options = {
-    host: '0.0.0.0',
-    port: PORT,
-  };
-  server.listen(options, () => {
+  server.listen(PORT, BIND, () => {
     logger.info(JSON.stringify({
       Server: 'Started',
-      'Listen on': `http://127.0.0.1:${options.port}`,
+      'Listen on': `http://127.0.0.1:${PORT}`,
     }, null, 2));
   });
 };
@@ -27,14 +25,10 @@ const httpsServer = () => {
     key: fs.readFileSync('jsx.jp/privkey.pem'),
   }, (req, res) => app(req, res));
   server.on('error', (e, req, res) => errorHandler(e, req, res));
-  const options = {
-    host: '0.0.0.0',
-    port: PORT + 1,
-  };
-  server.listen(options, () => {
+  server.listen(SPORT, BIND, () => {
     logger.info(JSON.stringify({
       Server: 'Started',
-      'Listen on': `https://127.0.0.1:${options.port}`,
+      'Listen on': `https://127.0.0.1:${SPORT}`,
     }, null, 2));
   });
 };
